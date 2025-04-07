17da00c7f9b816d822a7ea297e1940adfd340652
Durgesh Vishwakarma <vishwakarmadurgesh21@gmail.com>
2025-04-08 00:35:00 +0530

    validator correct it

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,47 +11,64 @@ import {
   getUserById,
   admins,
   resetPasswordRequest,
-  resetPassword,
+  resetPassword
 } from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 import validateRequest from '../middleware/validator.js';
-import userValidator from '../validators/userValidator.js'; // Moved validation logic to a separate file
+import {body, param} from 'express-validator';
 
 const router = express.Router();
+const validator = {
+  checkLogin: [
+    body('email').trim().notEmpty().withMessage('Email is Required').bail().isEmail().withMessage("Please enter a valid email address"),
+    body('password').trim().isString().notEmpty().withMessage('Password is Empty')
+  ],
+  checkNewUser: [
+    body('email').trim().notEmpty().withMessage('Email is Required').bail().isEmail().withMessage("Please enter a valid email address"),
+    body('password').trim().isString().notEmpty().withMessage('Password is Empty').bail()
+      .isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+    body('name').trim().notEmpty().withMessage('Name is Required').escape()
+  ],
+  checkGetUserById: [
+    param('id').exists().withMessage('Id is required').isMongoId().withMessage('Invalid Id')
+  ],
+  checkUpdateUser: [
+    body('email').trim().notEmpty().withMessage('Email is Required').bail().isEmail().withMessage("Please enter a valid email address"),
+    body('name').trim().notEmpty().withMessage('Name is Required').escape(),
+    body('isAdmin').isBoolean().withMessage('isAdmin value should be true/false'),
+    param('id').exists().withMessage('Id is required').isMongoId().withMessage('Invalid Id')
+  ],
+  resetPasswordRequest: [
+    body('email').trim().notEmpty().withMessage('Email is Required').bail().isEmail().withMessage("Please enter a valid email address")
+  ],
+  resetPassword: [
+    body('password').trim().notEmpty().withMessage('Password is Required').escape().bail()
+      .isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+    param('id').exists().withMessage('Id is required').isMongoId().withMessage('Invalid Id'),
+    param('token').trim().notEmpty().withMessage('Token is Required')
+  ]
+}
 
-// Public Routes
-router.post('/login', userValidator.checkLogin, validateRequest, loginUser);
+router.route('/')
+  .post(validator.checkNewUser, validateRequest, registerUser)
+  .get(protect, admin, getUsers);
+
+router.route('/admins').get(protect, admin, admins);
+
+router.post('/reset-password/request', validator.resetPasswordRequest, validateRequest, resetPasswordRequest);
+router.post('/reset-password/reset/:id/:token', validator.resetPassword, validateRequest, resetPassword);
+router.post('/login', validator.checkLogin, validateRequest, loginUser);
 router.post('/logout', protect, logoutUser);
-router.post(
-  '/reset-password/request',
-  userValidator.resetPasswordRequest,
-  validateRequest,
-  resetPasswordRequest
-);
-router.post(
-  '/reset-password/reset/:id/:token',
-  userValidator.resetPassword,
-  validateRequest,
-  resetPassword
-);
-router.post('/', userValidator.checkNewUser, validateRequest, registerUser);
 
-// Protected Routes (User)
 router
   .route('/profile')
   .get(protect, getUserProfile)
-  .put(protect, userValidator.checkNewUser, validateRequest, updateUserProfile);
-
-// Admin Routes
-router.get('/admins', protect, admin, admins);
-router
-  .route('/')
-  .get(protect, admin, getUsers);
+  .put(validator.checkNewUser, validateRequest, protect, updateUserProfile);
 
 router
   .route('/:id')
-  .get(protect, admin, userValidator.checkGetUserById, validateRequest, getUserById)
-  .put(protect, admin, userValidator.checkUpdateUser, validateRequest, updateUser)
-  .delete(protect, admin, userValidator.checkGetUserById, validateRequest, deleteUser);
+  .get(validator.checkGetUserById, validateRequest, protect, admin, getUserById)
+  .put(validator.checkUpdateUser, validateRequest, protect, admin, updateUser)
+  .delete(validator.checkGetUserById, validateRequest, protect, admin, deleteUser);
 
 export default router;
